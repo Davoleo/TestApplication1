b7107ec1d61f36c1ce5404c98e5631b199d565fa
Davoleo <dav@davoleo.net>
2021-10-06 19:19:46 +0200

    arrrrray

---
+++ src/structures/arrray.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <iostream>
+
+template<typename T, size_t S>
+class arrray
+{
+private:
+	T buffer[S];
+
+public:
+	//constexpr because it can be evauluated at compile time
+	constexpr unsigned int size() const {
+		return S;
+	}
+
+	T& operator[](unsigned int index) {
+		if (index >= S)
+			throw std::exception("Index out of bounds!")
+		return buffer[index];
+	}
+
+	//Overload for const arrays
+	const T& operator[](unsigned int index) const {
+		if (index >= S)
+			throw std::exception("Index out of bounds!")
+		return buffer[index];
+	}
+
+	//Returns the raw CArray Pointer
+	T* raw() {
+		return buffer;
+	}
+	const T* raw() const {
+		return buffer;
+	}
+};
+
